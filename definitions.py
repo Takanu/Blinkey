4e117561b032cc2af3606f873167da551ef223da
Takanu <crocadillian@me.com>
2015-09-01 14:01:13 +0100

    Fixed Selectable Bugs
    Fixed a bug where if you defined a filter as unselectable and tried to
    change the shading of it, all the objects defined by that filter would
    become selectable again.  I also made the code less messy…

--- definitions.py
+++ definitions.py
@@ -2,6 +2,100 @@ import bpy, bmesh, time
 from math import *
 from mathutils import Vector
 
+def FilterObjects(filterEntry, context):
+
+    nameFilterList = []
+    typeFilterList = []
+
+    scn = context.scene
+    user_preferences = context.user_preferences
+    addon_prefs = user_preferences.addons["Blinkey"].preferences
+
+    # Figure out which category the request came from
+    preset = addon_prefs.presets[addon_prefs.presets_index]
+    category = None
+
+    categoryName = filterEntry.name
+    shading = filterEntry.visibility
+
+    for item in preset.categories:
+        if item.name == categoryName:
+            category = item
+
+    print(">>> Updating", filterEntry.name, "Category <<<")
+    print(">>> Checking Name Filter <<<")
+
+    # Now collect objects based on the filtering categories
+    for object in bpy.data.objects:
+        print("Found object...", object.name)
+
+        if category.name_filter != "":
+            if category.name_filter_type is '1':
+                if CheckSuffix(object.name, category.name_filter) is True:
+                    print("Object matches name filter")
+                    nameFilterList.append(object)
+
+            elif category.name_filter_type is '2':
+                if object.name.find(category.name_filter) == 0:
+                    print("Object matches name filter")
+                    nameFilterList.append(object)
+
+            elif category.name_filter_type is '3':
+                if object.name.find(category.name_filter) != -1:
+                    print("Object matches name filter")
+                    nameFilterList.append(object)
+
+        else:
+            print("Object matches name filter")
+            nameFilterList.append(object)
+
+    print("Current object list...", len(nameFilterList))
+    print("Checking Type Filter.....", category.object_type)
+
+    typeFilter = 'NONE'
+
+    if category.object_type == '1':
+        typeFilter = 'NONE'
+    elif category.object_type == '2':
+        typeFilter = 'MESH'
+    elif category.object_type == '3':
+        typeFilter = 'CURVE'
+    elif category.object_type == '4':
+        typeFilter = 'SURFACE'
+    elif category.object_type == '5':
+        typeFilter = 'META'
+    elif category.object_type == '6':
+        typeFilter = 'FONT'
+    elif category.object_type == '7':
+        typeFilter = 'ARMATURE'
+    elif category.object_type == '8':
+        typeFilter = 'LATTICE'
+    elif category.object_type == '9':
+        typeFilter = 'EMPTY'
+    elif category.object_type == '10':
+        typeFilter = 'CAMERA'
+    elif category.object_type == '11':
+        typeFilter = 'LAMP'
+    elif category.object_type == '12':
+        typeFilter = 'SPEAKER'
+
+    print("Type Filter Found...", typeFilter)
+
+    if typeFilter == 'NONE':
+        for object in nameFilterList:
+            print("Checking object...", object.name)
+            print("Object matches type filter")
+            typeFilterList.append(object)
+
+    else:
+        for object in nameFilterList:
+            print("Checking object...", object.name)
+            if object.type == typeFilter:
+                print("Object matches type filter")
+                typeFilterList.append(object)
+
+    return typeFilterList
+
 def FocusObject(target):
 
     # If the target isnt visible, MAKE IT FUCKING VISIBLE.
--- update.py
+++ update.py
@@ -1,7 +1,7 @@
 import bpy, bmesh, time
 from math import *
 
-from .definitions import FocusObject, SelectObject, GenerateObjectShading, CheckSuffix
+from .definitions import FilterObjects, FocusObject, SelectObject, GenerateObjectShading, CheckSuffix
 
 def Update_VisibilityCategory(self, context):
 
@@ -22,95 +22,17 @@ def Update_VisibilityCategory(self, context):
     mode = bpy.context.mode
     bpy.ops.object.mode_set(mode='OBJECT')
 
-    categoryName = self.name
-    shading = self.visibility
+    #Now filter through and shade the objects
+    finalList = []
+    finalList = FilterObjects(self, context)
 
-    # Figure out which category the request came from
-    preset = addon_prefs.presets[addon_prefs.presets_index]
-    category = None
-
-    for item in preset.categories:
-        if item.name == categoryName:
-            category = item
-
-    nameFilterList = []
-    typeFilterList = []
-
-    print(">>> Updating", self.name, "Category <<<")
-    print(">>> Checking Name Filter <<<")
-
-    # Now collect objects based on the filtering categories
-    for object in bpy.data.objects:
-        print("Found object...", object.name)
-
-        if category.name_filter != "":
-            if category.name_filter_type is '1':
-                if CheckSuffix(object.name, category.name_filter) is True:
-                    print("Object matches name filter")
-                    nameFilterList.append(object)
-
-            elif category.name_filter_type is '2':
-                if object.name.find(category.name_filter) == 0:
-                    print("Object matches name filter")
-                    nameFilterList.append(object)
-
-            elif category.name_filter_type is '3':
-                if object.name.find(category.name_filter) != -1:
-                    print("Object matches name filter")
-                    nameFilterList.append(object)
-
-        else:
-            print("Object matches name filter")
-            nameFilterList.append(object)
-
-    print("Current object list...", len(nameFilterList))
-    print("Checking Type Filter.....", category.object_type)
-
-    typeFilter = 'NONE'
-
-    if category.object_type == '1':
-        typeFilter = 'NONE'
-    elif category.object_type == '2':
-        typeFilter = 'MESH'
-    elif category.object_type == '3':
-        typeFilter = 'CURVE'
-    elif category.object_type == '4':
-        typeFilter = 'SURFACE'
-    elif category.object_type == '5':
-        typeFilter = 'META'
-    elif category.object_type == '6':
-        typeFilter = 'FONT'
-    elif category.object_type == '7':
-        typeFilter = 'ARMATURE'
-    elif category.object_type == '8':
-        typeFilter = 'LATTICE'
-    elif category.object_type == '9':
-        typeFilter = 'EMPTY'
-    elif category.object_type == '10':
-        typeFilter = 'CAMERA'
-    elif category.object_type == '11':
-        typeFilter = 'LAMP'
-    elif category.object_type == '12':
-        typeFilter = 'SPEAKER'
-
-    print("Type Filter Found...", typeFilter)
-
-    if typeFilter == 'NONE':
-        for object in nameFilterList:
-            print("Checking object...", object.name)
-            print("Object matches type filter")
-            typeFilterList.append(object)
-
-    else:
-        for object in nameFilterList:
-            print("Checking object...", object.name)
-            if object.type == typeFilter:
-                print("Object matches type filter")
-                typeFilterList.append(object)
-
-    for finalItem in typeFilterList:
+    for finalItem in finalList:
         print("Processing shading....", finalItem.name)
-        GenerateObjectShading(finalItem, shading)
+        GenerateObjectShading(finalItem, self.visibility)
+
+    if self.selectable is False:
+        for finalItem in finalList:
+            finalItem.hide_select = True
 
     # Re-select the objects previously selected
     if active is not None:
@@ -145,81 +67,16 @@ def Update_Selectable(self, context):
     mode = bpy.context.mode
     bpy.ops.object.mode_set(mode='OBJECT')
 
-    categoryName = self.name
-    shading = self.visibility
-
-    # Figure out which category the request came from
-    preset = addon_prefs.presets[addon_prefs.presets_index]
-    category = None
-
-    for item in preset.categories:
-        if item.name == categoryName:
-            category = item
-
-    nameFilterList = []
-    typeFilterList = []
-
-    # Now collect objects based on the filtering categories
-    for object in bpy.data.objects:
-
-        if category.name_filter != "":
-            if category.name_filter_type is '1':
-                if CheckSuffix(object.name, category.name_filter) is True:
-                    nameFilterList.append(object)
-
-            elif category.name_filter_type is '2':
-                if object.name.find(category.name_filter) == 0:
-                    nameFilterList.append(object)
-
-            elif category.name_filter_type is '3':
-                if object.name.find(category.name_filter) != -1:
-                    nameFilterList.append(object)
-
-        else:
-            nameFilterList.append(object)
-
-    typeFilter = 'NONE'
-
-    if category.object_type == '1':
-        typeFilter = 'NONE'
-    elif category.object_type == '2':
-        typeFilter = 'MESH'
-    elif category.object_type == '3':
-        typeFilter = 'CURVE'
-    elif category.object_type == '4':
-        typeFilter = 'SURFACE'
-    elif category.object_type == '5':
-        typeFilter = 'META'
-    elif category.object_type == '6':
-        typeFilter = 'FONT'
-    elif category.object_type == '7':
-        typeFilter = 'ARMATURE'
-    elif category.object_type == '8':
-        typeFilter = 'LATTICE'
-    elif category.object_type == '9':
-        typeFilter = 'EMPTY'
-    elif category.object_type == '10':
-        typeFilter = 'CAMERA'
-    elif category.object_type == '11':
-        typeFilter = 'LAMP'
-    elif category.object_type == '12':
-        typeFilter = 'SPEAKER'
-
-    if typeFilter == 'NONE':
-        for object in nameFilterList:
-            typeFilterList.append(object)
-
-    else:
-        for object in nameFilterList:
-            if object.type == typeFilter:
-                typeFilterList.append(object)
+    #Now filter through and shade the objects
+    finalList = []
+    finalList = FilterObjects(self, context)
 
     isSelectable = True
 
-    if category.selectable is True:
+    if self.selectable is True:
         isSelectable = False
 
-    for finalItem in typeFilterList:
+    for finalItem in finalList:
         print("Editing selection....", finalItem.name)
         finalItem.hide_select = isSelectable
 
